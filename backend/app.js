9651f630fa2dba753f0699fe1a3fe18f81a4ca93
amit.dhakad <amit.dhakad@gmail.com>
2018-12-03 21:25:06 +0530

    mongodb configuration

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+// eqHsctsixIdvUpyl
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
