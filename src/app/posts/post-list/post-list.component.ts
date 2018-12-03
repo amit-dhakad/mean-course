990d409510b201d30e48dbbc577656836e091fc4
amit.dhakad <amit.dhakad@gmail.com>
2018-12-03 12:32:29 +0530

    create post and post list component

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Post } from '../post.model';
+import { PostService } from '../posts.service';
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.scss']
+})
+export class PostListComponent implements OnInit, OnDestroy {
+  /*  posts = [
+    {title: 'First Post', content: ' This is the first post\'s content '},
+    {title: 'Second Post', content: ' This is the second post\'s content '},
+    {title: 'Third Post', content: ' This is the third post\'s content '}
+  ]; */
+
+  posts: Post[] = [];
+  private postSub: Subscription;
+  constructor(public postsService: PostService) {}
+
+  ngOnInit() {
+    this.posts = this.postsService.getPost();
+    this.postSub = this.postsService
+      .getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
+
+  ngOnDestroy() {
+    this.postSub.unsubscribe();
+  }
+}
